dd605d87575393dc60dee6781ab68566fa225167
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-11-24 23:22:28 +0100

    Add missing helper to fix exception.

--- app/controllers/eu_gdpr/backend/home_controller.rb
+++ app/controllers/eu_gdpr/backend/home_controller.rb
@@ -1,7 +1,7 @@
 module EuGdpr
   module Backend
     class HomeController < Administrador::HomeController::Base
-      # helper Rao::Component::TableHelper
+      helper Rao::Component::TableHelper
     end
   end
 end
